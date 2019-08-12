0373e7947ca2938f6354d66baeaa1d457dd61a0f
AADITYA <aaditya@ip-192-168-1-123.ap-south-1.compute.internal>
2019-08-12 16:37:33 +0530

    testing feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there, aaditya 
+          Hi there, aaditya , wassup
         </p>
         <a
           className="App-link"
